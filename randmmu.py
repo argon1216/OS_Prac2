f7ed0f689c9c1af500a77a79a2c1cb233853dba9
dylan <dylan.gray546@gmail.com>
2023-08-28 15:41:39 +0930

    Added the randomMMU section,haent tested yet going to wait for test cases please check

--- randmmu.py
+++ randmmu.py
@@ -1,34 +1,63 @@
 from mmu import MMU
+import random
+class Page:
+    def __init__(self,number: int, dirty: bool):
+        self.number = number
+        self.dirty = dirty
+        
 
 class RandMMU(MMU):
     def __init__(self, frames):
         # TODO: Constructor logic for RandMMU
-        pass
+        self.frames = frames
+        self.memory = {}
+        self.disk_reads = 0
+        self.disk_writes = 0
+        self.page_faults = 0
+        self.debug_mode = False
 
     def set_debug(self):
         # TODO: Implement the method to set debug mode
+        self.debug_mode = True
         pass
 
     def reset_debug(self):
         # TODO: Implement the method to reset debug mode
+        self.debug_mode = False
         pass
 
     def read_memory(self, page_number):
         # TODO: Implement the method to read memory
-        pass
+        if page_number not in self.memory:
+            self.page_faults += 1
+
+            if len(self.memory) >= self.frames:
+                page_to_replace = random.choice(list(self.memory.keys()))
+                if self.memory[page_to_replace].dirty:
+                    self.disk_writes += 1
+                del self.memory[page_to_replace]
+                if self.debug_mode:
+                    print(f"Replace page {page_to_replace} (random)")
+            self.memory[page_number] = Page(page_number, False)
+        
+        if self.debug_mode:
+            print(f"Reading page {page_number}")
 
     def write_memory(self, page_number):
         # TODO: Implement the method to write memory
-        pass
+        self.read_memory(page_number)
+        self.memory[page_number].dirty = True
+        if self.debug_mode:
+            print(f"Write page {page_number}")
 
     def get_total_disk_reads(self):
         # TODO: Implement the method to get total disk reads
-        return -1
+        return self.disk_reads
 
     def get_total_disk_writes(self):
         # TODO: Implement the method to get total disk writes
-        return -1
+        return self.disk_writes
 
     def get_total_page_faults(self):
         # TODO: Implement the method to get total page faults
-        return -1
+        return self.page_faults
